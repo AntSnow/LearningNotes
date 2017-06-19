868f5a6ccf2f53a52312c01e22fe9e80f2a60232
马义飞 <iosmayifei@163.com>
2017-06-19 18:26:58 +0800

    小更新

--- LearningNotes/LearningNotes/ViewController.swift
+++ LearningNotes/LearningNotes/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let vc = ATMainViewController.init()
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-
-
 }
 
